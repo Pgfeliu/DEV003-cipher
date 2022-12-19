b4fab2f1bb6524c72df08a454deb59f66280215f
Pgfeliu <pamela.gfeliu@gmail.com>
2022-12-19 14:39:18 -0300

    add proyect

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import cipher from './cipher.js';
 
-/*b1 = botón 1,
-  b2 = botón 2,
-  t1 = texto 1, (mensaje original no cifrado),
-  t2 = texto 2, (mensaje cifrado)*/
 
 //definiendo el botón codificar//
-let encode = document.getElementById("encode");
+const encode = document.getElementById("encode");
 
 //Creando función flecha anónima dentro de un evento click que responda al botón "cifrar"  
 encode.addEventListener("click", () => { 
 //lo que hará cuando haga el click es lo siguiente://
-    let box1= document.getElementById("cuadro1").value;
-    let offset = document.getElementById("offset").value;
-    let textoCodificado = cipher.encode(offset, box1);
+  const mensaje1= document.getElementById("text1").value;
+  const offset = document.getElementById("offset").value;
+  const textoCodificado = cipher.encode(offset, mensaje1);
+  console.log(textoCodificado);
 
-    document.getElementsById("cuadro3").innerHTML = textoCodificado;
+  document.getElementById("text3").value = textoCodificado;
 });
 
 
 //definiendo el botón decodificar//
-let decode = document.getElementById("decode");
+const decode = document.getElementById("decode");
 
 //Creando función flecha anónima dentro de un evento click que responda al botón "descifrar"
 decode.addEventListener("click", () => {
-    let box3 = document.getElementsById("cuadro3").value;
-    let offset= document.getElementById("offset").value;
-    let textoDecodificado = cipher.decode(offset, box3);
-  textoDecodificado = document.getElementById("cuadro1");
-    //document.getElementsById("cuadro1").innerHTML = textoDecodificado;
+  const mensaje3 = document.getElementById("text3").value;
+  const offset= document.getElementById("offset").value;
+  const textoDecodificado = cipher.decode(offset, mensaje3);
+ 
+  document.getElementById('text1').value = textoDecodificado;
+
 });
